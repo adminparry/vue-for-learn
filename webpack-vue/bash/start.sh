6397652aa44bf65e6b7f5d0fa4adeb118dcdb58f
pparry <1170015014@qq.com>
2020-03-02 14:18:12 +0800

    start

---
+++ webpack-vue/bash/start.sh
@@ -0,0 +1,16 @@
+#建立工程目录
+mkdir dist src webpack
+#初始化依赖
+npm init -y
+npm install vue
+npm install --save-dev html-webpack-plugin vue-loader vue-template-compiler webpack webpack-cli webpack-dev-server webpack-merge
+#创建babel配置和入口模板html
+touch index.html .babelrc
+#创建入口文件和组件文件夹
+cd src 
+touch main.js
+mkdir components
+cd ..
+#webpack配置
+cd webpack
+touch eo.js loader.js dev.js
